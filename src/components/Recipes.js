e878f9046d491358682c1c14ab4a7c202698ab97
orijinnCHD <reussir.startup@gmail.com>
2022-08-30 08:16:28 +0400

    Sass completed

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import Card from './Card';
 
 const Recipes = () => {
+
+    useEffect(()=>{
+
+        axios.get('https://restcountries.com/v3.1/all').then((res)=>{console.log(res.data)});
+    
+    },[])
+
     return (
         <ul className='recipes'>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
             <Card/>
             <Card/>
             <Card/>
