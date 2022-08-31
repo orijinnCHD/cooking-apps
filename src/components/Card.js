1508fb89f91a00ee079fee91a3015ae79e6f7ecd
orijinnCHD <reussir.startup@gmail.com>
2022-08-31 15:48:05 +0400

    final commit 2 : clamp line paragraph card

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,14 +9,7 @@ const Card = ({meal}) => {
             <h2>{meal.strMeal}</h2>
             <h4>origin : {meal.strArea}</h4>
             <img src={meal.strMealThumb} alt={meal.strMeal} />
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                Quaerat cum maxime accusantium blanditiis dolorum asperiores velit. 
-                Repudiandae, iure id perferendis qui eligendi illo reprehenderit laboriosam 
-                iusto doloremque optio veritatis vitae, quod ut porro ab, vero inventore 
-                soluta consequatur officiis? Odio, saepe reprehenderit. Exercitationem 
-                veritatis officia nemo, rem alias tenetur autem fugiat, asperiores 
-                minima itaque recusandae voluptates, cum rerum fuga reprehenderit.
-            </p>
+            <p>{meal.strInstructions}</p>
 
         </li>
     );
